dd0f1f5a454524da5cbd706f3496a2ec4f027a62
Rabah Jamal <rabah9686@gmail.com>
2018-07-19 18:05:04 +0200

    Refactor validate method

--- java/mtax/MTax.java
+++ java/mtax/MTax.java
@@ -12,69 +12,66 @@ public class MTax implements Constant {
         
         List<String> errorList = new ArrayList<>();
         
-//        List<String> taxCategoryList = MInfoTaxCategory.getTaxCategoryStringList();
-        
         if(xTaxList != null && xTaxList.size() > 0) {
             List<String> validIds = new ArrayList<>();
             int cont = 0;
             for (X_Tax tax : xTaxList) {
-                if(tax.getId() != null){
+                final boolean taxId = (tax.getId() != null);
+                final boolean nullTax = (tax.getTax() == null);
+                final boolean notLocalTax = (!tax.isLocal());
+
+                if(taxId){
                     validIds.add(tax.getId().toString());
                 }
-//                if(tax.getAmount() == null) {
-//                    errorList.add("El importe es obligatorio");
-//                }
 
-                if(tax.getTax() == null) {
+                if(nullTax) {
                     errorList.add("El impuesto es obligatorio");
                 }
-//                else if(!taxCategoryList.contains(tax.getTax())) {
-//                    errorList.add("El impuesto no es un dato valido");
-//                }
-
-//                if(tax.isLocal()){
-//                    if(tax.isTrasladado() && tax.getTaxAmount() == null ) {
-//                        errorList.add("El importe es obligatorio");
-//                    }
-//                } 
-//                else {
-//                    if(tax.getTaxAmount() == null ) {
-//                        errorList.add("El importe es obligatorio");
-//                    }
-//                }
                 
-                if(!tax.isLocal()){
+                if(notLocalTax){
                     cont++;
                 }
             }
-            if(cont<=0){
+            
+            final boolean contLessThanOrEqualZero = (cont<=0);
+            final boolean validIdsSizeGreaterThanZero = (validIds.size() > 0);
+
+            if(contLessThanOrEqualZero){
                 errorList.add("Debe de incluir al menos una tasa no local");
             }
-            if(validIds.size() > 0){
+            if(validIdsSizeGreaterThanZero){
                     
                     List<X_Tax> xt = TaxsByListId(validIds, false);
-                    if(xt.size() != validIds.size()){
+                    final boolean notEqualSizes = (xt.size() != validIds.size());
+
+                    if(notEqualSizes){
                         errorList.add("Existen datos no guardados previamente");
                     }else{
-                        HashMap<String, X_Tax> map_taxs = new HashMap<String, X_Tax>();
-                        for(X_Tax tax: xt){
-                            map_taxs.put(tax.getId().toString(), tax);
-                        }
-                        for(int i = 0; i < xTaxList.size(); i++){
-                            if(xTaxList.get(i).getId() != null){
-                                xTaxList.get(i).setCreated(
-                                        map_taxs.get(xTaxList.get(i).getId().toString())
-                                                .getCreated());
-                            }
-                        }
+                        HashMap<String, X_Tax> map_taxs = getMapTaxs(xt);
+                        updateXTaxList(xTaxList, map_taxs);
                     }
             }
         }
-//        else {
-//            errorList.add("El documento no tiene tasas");
-//        }
         
         return errorList;
+    }
+
+    public static HashMap<String, X_Tax> getMapTaxs(List<X_Tax> xt) {
+        HashMap<String, X_Tax> map_taxs = new HashMap<String, X_Tax>();
+
+        for(X_Tax tax: xt){
+            map_taxs.put(tax.getId().toString(), tax);
+        }
+        
+        return map_taxs;
+    }
+
+    public static void updateXTaxList(List<X_Tax> xTaxList, HashMap<String, X_Tax> map_taxs) {
+        for(X_Tax tax: xTaxList) {
+          if(tax.getId() != null) {
+            tax.setCreated(map_taxs.get(tax.getId().toString()).getCreated());
+          }
+        }
     }
     
 }
